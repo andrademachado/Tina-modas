9b998fc106b4dcd6a9ff91179cf1d67497d3cc02
Lígia Andrade <andradeligiamp@gmail.com>
2024-09-30 17:18:09 -0300

    Construção das variáveis da tabela de cores no Estilo Global

--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalCss = createGlobalStyle`
+//Variávei das cores do todo o Projeto
+const cores = {
+    branco:' #F9F9F9',
+    marssala:'#6b141b',
+    preto:' #050404',
+    cinzaclaro:'#DCD2D3',
+    CinzaEscuro:'#907671'
+}
+
+//reset do estilo global
+export const GlobalCss = createGlobalStyle`
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font-family: Roboto, sans-serif;
     }
+
+    body {
+        background-color: ${cores.branco};
+        color: ${cores.marssala};
+    }
 `
+
+
